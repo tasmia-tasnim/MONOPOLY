e8ec0c7a92e2e00c512f1f052ded86de53fc86b7
tasmia-tasnim <tasmia.tasnim@g.bracu.ac.bd>
2025-08-26 05:17:00 +0600

    Create gameRoutes.js

---
+++ gameRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const GameController = require('../controllers/GameController');
+
+// Game management 
+router.post('/create', GameController.createGame);
+router.get('/:gameId', GameController.getGameState);
+router.delete('/:gameId', GameController.deleteGame);
+router.get('/:gameId/statistics', GameController.getGameStatistics);
+
+// Game actions
+router.post('/:gameId/roll-dice', GameController.rollDice);
+router.post('/:gameId/end-turn', GameController.endTurn);
+router.post('/:gameId/buy-property', GameController.buyProperty);
+router.post('/:gameId/mortgage-property', GameController.mortgageProperty);
+router.post('/:gameId/build-house', GameController.buildHouse);
+
+// Property 
+router.get('/property/:propertyId', GameController.getProperty);
+
+module.exports = router;
